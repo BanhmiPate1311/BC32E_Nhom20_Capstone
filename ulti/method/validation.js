ef4a92cb7e3d9c655324d3bdc5c5f08466903924
BanhmiPate1311 <huynhcuong@gmail.com>
2022-08-12 17:57:22 +0700

    update

---
+++ ulti/method/validation.js
@@ -0,0 +1,75 @@
+let form = document.querySelector("#registerForm ");
+console.log(form);
+form.addEventListener("input", function (e) {
+  switch (e.target.id) {
+    case "email":
+      var valid = checkEmail();
+      break;
+    case "password":
+      var valid = checkPassword();
+      break;
+    case "password-confirm":
+      var valid = checkPassWordConFirm();
+      break;
+    case "name":
+      var valid = checkName();
+      break;
+    case "phone":
+      var valid = checkPhone();
+      break;
+  }
+  //   if (!valid) {
+  //     document.querySelector("#" + e.target.id).style.borderColor = "#f44336";
+  //   } else {
+  //     document.getElementById(e.target.id).style.borderColor = "none";
+  //   }
+});
+
+function checkEmail() {
+  let value = document.querySelector("#email").value;
+  let valid = true;
+  valid &=
+    kiemTraEmail(value, "#tbEmail", "Email") &
+    kiemTraRong(value, "#tbEmail", "Email");
+  return valid;
+}
+
+function checkPassword() {
+  let value = document.querySelector("#password").value;
+  let valid = true;
+  valid &=
+    kiemTraMatKhau(value, "#tbPassWord", "Mật khẩu", 6, 10) &
+    kiemTraRong(value, "#tbPassWord", "Mật khẩu");
+  return valid;
+}
+
+function checkPassWordConFirm() {
+  let value1 = document.querySelector("#password").value;
+  let value2 = document.querySelector("#password-confirm").value;
+  let valid = true;
+  valid = kiemtraPassWordConFirm(
+    value1,
+    value2,
+    "#tbPassWordConFirm",
+    "Mật khẩu"
+  );
+  return valid;
+}
+
+function checkName() {
+  let value = document.querySelector("#name").value;
+  let valid = true;
+  valid &=
+    kiemTraTatCaKyTu(value, "#tbName", "Họ và tên") &
+    kiemTraRong(value, "#tbName", "Họ và tên");
+  return valid;
+}
+
+function checkPhone() {
+  let value = document.querySelector("#phone").value;
+  let valid = true;
+  valid &=
+    kiemTraPhone(value, "#tbPhone", "Số Phone") &
+    kiemTraRong(value, "#tbPhone", "Số Phone");
+  return valid;
+}
